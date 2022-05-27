ddd0e809efdb01b8dab4a053715c8080b3492137
ManhcuongG1 <tranmanhcuongqbdl1@gmail.com>
2022-05-27 07:57:53 +0700

    first commit

--- ss9_application/bai_tap/calculator/src/main/java/CalculatorServlet.java
+++ ss9_application/bai_tap/calculator/src/main/java/CalculatorServlet.java
@@ -9,21 +9,26 @@ import java.io.PrintWriter;
 @WebServlet(name = "CalculatorServlet",urlPatterns = "/calculator")
 public class CalculatorServlet extends javax.servlet.http. HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String description = request.getParameter("description");
-        float price = Float.parseFloat(request.getParameter("price"));
-        int discount = Integer.parseInt(request.getParameter("discount"));
-        float amount = (float) ( price *discount *0.01);
-        float discountPrice = price-amount;
+        try{
+            String description = request.getParameter("description");
+            float price = Float.parseFloat(request.getParameter("price"));
+            int discount = Integer.parseInt(request.getParameter("discount"));
+            float amount = (float) ( price *discount *0.01);
+            float discountPrice = price-amount;
 
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        writer.println("<head>");
-        writer.println(" <meta charset=\"UTF-8\">");
-        writer.println("</head>");
-        writer.println("<h1>Lượng chiết khấu: " + amount +"</h1>");
-        writer.println("<h1>Giá sau khi chiết khấu: " + discountPrice +"</h1>");
+            PrintWriter writer = response.getWriter();
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println(" <meta charset=\"UTF-8\">");
+            writer.println("</head>");
+            writer.println("<h1>Lượng chiết khấu: " + amount +"</h1>");
+            writer.println("<h1>Giá sau khi chiết khấu: " + discountPrice +"</h1>");
 
-        writer.println("</html>");
+            writer.println("</html>");
+
+        }catch (NumberFormatException e){
+            request.getRequestDispatcher("error.jsp").forward(request,response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ ss9_application/bai_tap/calculator/src/main/webapp/error.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: TechCare
+  Date: 5/27/2022
+  Time: 7:50 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<div>
+<img src="https://bizflyportal.mediacdn.vn/bizflyportal/461/347/2020/06/02/22/54/nhi15910916872906.jpg" alt="" height="100%" width="100%">
+</div>
+</body>
+</html>
